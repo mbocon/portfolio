23d3ddd01ac1468789da9ff0a2cdda865fa144a0
Michael Bocon <mikebocon@gmail.com>
2021-07-31 20:58:06 -1000

    updates index.css

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,96 +1,104 @@
-import React from 'react';
+import React from "react";
 
 function Skills() {
-	return (
-		<div className='resume-section' id='skills' style={{ height: '100vh' }}>
-			<section className='resume-section' id='skills'>
-				<div className='resume-section-content'>
-					<h2 className='mb-5'>Skills</h2>
-					<div className='subheading mb-3'>Programming Languages & Tools</div>
-					<ul className='list-inline dev-icons'>
-						<li className='list-inline-item'>
-							<i className='devicon-html5-plain'></i>
-						</li>
-						<li className='list-inline-item'>
-							<i className='devicon-css3-plain'></i>
-						</li>
-						<li className='list-inline-item'>
-							<i className='devicon-bootstrap-plain'></i>
-						</li>
-						<li className='list-inline-item'>
-							<i className='fab fa-font-awesome-flag'></i>
-						</li>
-						<li className='list-inline-item'>
-							<i className='devicon-sass-original'></i>
-						</li>
-						<li className='list-inline-item'>
-							<i className='devicon-devicon-plain'></i>
-						</li>
-						<li className='list-inline-item'>
-							<i className='devicon-javascript-plain'></i>
-						</li>
-						<li className='list-inline-item'>
-							<i className='devicon-jquery-plain'></i>
-						</li>
-						<li className='list-inline-item'>
-							<i className='devicon-react-original'></i>
-						</li>
-						<li className='list-inline-item'>
-							<i className='devicon-ruby-plain'></i>
-						</li>
-						<li className='list-inline-item'>
-							<i className='devicon-rails-plain'></i>
-						</li>
-						<li className='list-inline-item'>
-							<i className='devicon-nodejs-plain'></i>
-						</li>
-						<li className='list-inline-item'>
-							<i className='devicon-express-original'></i>
-						</li>
-						<li className='list-inline-item'>
-							<i className='devicon-mongodb-plain'></i>
-						</li>
-						<li className='list-inline-item'>
-							<i className='devicon-postgresql-plain'></i>
-						</li>
-						<li className='list-inline-item'>
-							<i className='devicon-git-plain'></i>
-						</li>
-						<li className='list-inline-item'>
-							<i className='devicon-github-plain'></i>
-						</li>
-						<li className='list-inline-item'>
-							<i className='devicon-heroku-original'></i>
-						</li>
-						<li className='list-inline-item'>
-							<i className='fab fa-npm'></i>
-						</li>
-					</ul>
-					<div className='subheading mb-3'>Workflow</div>
-					<ul className='fa-ul mb-0'>
-						<li>
-							<span className='fa-li'>
-								<i className='fas fa-check'></i>
-							</span>
-							Mobile-First, Responsive Design
-						</li>
-						<li>
-							<span className='fa-li'>
-								<i className='fas fa-check'></i>
-							</span>
-							Cross Browser Testing & Debugging
-						</li>
-						<li>
-							<span className='fa-li'>
-								<i className='fas fa-check'></i>
-							</span>
-							Agile Development & Scrum
-						</li>
-					</ul>
-				</div>
-			</section>
-		</div>
-	);
+   return (
+      <div className="resume-section" id="skills" style={{ height: "100vh" }}>
+         <section className="resume-section" id="skills">
+            <div className="resume-section-content">
+               <h2 className="mb-5">Skills</h2>
+               <div className="subheading mb-3">
+                  Programming Languages & Tools
+               </div>
+               <ul className="list-inline dev-icons">
+                  <li className="list-inline-item">
+                     <i className="devicon-html5-plain"></i>
+                  </li>
+                  <li className="list-inline-item">
+                     <i className="devicon-css3-plain"></i>
+                  </li>
+                  <li className="list-inline-item">
+                     <i className="devicon-bootstrap-plain"></i>
+                  </li>
+                  <li className="list-inline-item">
+                     <i className="fab fa-font-awesome-flag"></i>
+                  </li>
+                  <li className="list-inline-item">
+                     <i className="devicon-sass-original"></i>
+                  </li>
+                  <li className="list-inline-item">
+                     <i className="devicon-devicon-plain"></i>
+                  </li>
+                  <li className="list-inline-item">
+                     <i className="devicon-javascript-plain"></i>
+                  </li>
+                  <li className="list-inline-item">
+                     <i className="devicon-jquery-plain"></i>
+                  </li>
+                  <li className="list-inline-item">
+                     <i className="devicon-react-original"></i>
+                  </li>
+                  <li className="list-inline-item">
+                     <i className="devicon-ruby-plain"></i>
+                  </li>
+                  <li className="list-inline-item">
+                     <i className="devicon-rails-plain"></i>
+                  </li>
+                  <li className="list-inline-item">
+                     <i className="devicon-nodejs-plain"></i>
+                  </li>
+                  <li className="list-inline-item">
+                     <i className="devicon-express-original"></i>
+                  </li>
+                  <li className="list-inline-item">
+                     <i className="devicon-mongodb-plain"></i>
+                  </li>
+                  <li className="list-inline-item">
+                     <i className="devicon-postgresql-plain"></i>
+                  </li>
+                  <li className="list-inline-item">
+                     <i className="devicon-git-plain"></i>
+                  </li>
+                  <li className="list-inline-item">
+                     <i className="devicon-github-plain"></i>
+                  </li>
+                  <li className="list-inline-item">
+                     <i className="devicon-heroku-original"></i>
+                  </li>
+                  <li className="list-inline-item">
+                     <i className="fab fa-npm"></i>
+                  </li>
+                  <li className="list-inline-item">
+                     <i className="devicon-python-plain"></i>
+                  </li>
+                  <li className="list-inline-item">
+                     <i className="devicon-django-plain"></i>
+                  </li>
+               </ul>
+               <div className="subheading mb-3">Workflow</div>
+               <ul className="fa-ul mb-0">
+                  <li>
+                     <span className="fa-li">
+                        <i className="fas fa-check"></i>
+                     </span>
+                     Mobile-First, Responsive Design
+                  </li>
+                  <li>
+                     <span className="fa-li">
+                        <i className="fas fa-check"></i>
+                     </span>
+                     Cross Browser Testing & Debugging
+                  </li>
+                  <li>
+                     <span className="fa-li">
+                        <i className="fas fa-check"></i>
+                     </span>
+                     Agile Development & Scrum
+                  </li>
+               </ul>
+            </div>
+         </section>
+      </div>
+   );
 }
 
 export default Skills;
